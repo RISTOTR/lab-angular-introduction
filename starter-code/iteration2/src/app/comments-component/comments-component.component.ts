e70cba4e9e3f540f813ccf8975f097369abd6444
RISTO TAPANI RAIHALA <tyokone@RISTOs-MacBook-Pro.local>
2018-05-01 12:44:53 +0200

    ngclass added to iteration2

--- starter-code/iteration2/src/app/comments-component/comments-component.component.ts
+++ starter-code/iteration2/src/app/comments-component/comments-component.component.ts
@@ -6,12 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./comments-component.component.css']
 })
 export class CommentsComponentComponent  {
-  bobby ="Bobby Tables";
-  date ="19/07/2018 19:30";
-  ironhack ="Ironhack School";
-  
+  Comment =[
+    {
+      name : "Boby Tables",
+      date: "19/07/2016",
+      hour: "19:30",
+      commentUser: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id est commodo, porttitor nunc in, iaculis ligula. Pellentesque euismod imperdiet egestas. Phasellus malesuada cursus rhoncus. Aliquam sit amet orci vitae turpis sagittis elementum. Ut ullamcorper eu nibh eget consequat"
+    },
+    {
+      name : "Ironhack School",
+      date: "19/07/2016",
+      hour: "20:35",
+      commentUser: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id est commodo, porttitor nunc in, iaculis ligula. Pellentesque euismod imperdiet egestas. Phasellus malesuada cursus rhoncus. Aliquam sit amet orci vitae turpis sagittis elementum. Ut ullamcorper eu nibh eget consequat"
+    },
+    {
+      name : "Boby Tables",
+      date: "19/07/2016",
+      hour: "19:30",
+      commentUser: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id est commodo, porttitor nunc in, iaculis ligula. Pellentesque euismod imperdiet egestas. Phasellus malesuada cursus rhoncus. Aliquam sit amet orci vitae turpis sagittis elementum. Ut ullamcorper eu nibh eget consequat"
+    },
+    {
+      name : "Ironhack School",
+      date: "19/07/2016",
+      hour: "20:35",
+      commentUser: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id est commodo, porttitor nunc in, iaculis ligula. Pellentesque euismod imperdiet egestas. Phasellus malesuada cursus rhoncus. Aliquam sit amet orci vitae turpis sagittis elementum. Ut ullamcorper eu nibh eget consequat"
+    },
+    {
+      name : "Boby Tables",
+      date: "19/07/2016",
+      hour: "19:30",
+      commentUser: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id est commodo, porttitor nunc in, iaculis ligula. Pellentesque euismod imperdiet egestas. Phasellus malesuada cursus rhoncus. Aliquam sit amet orci vitae turpis sagittis elementum. Ut ullamcorper eu nibh eget consequat"
+    }
 
- 
-  
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
+
+
+
